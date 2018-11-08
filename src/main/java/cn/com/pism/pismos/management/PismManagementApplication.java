33520b9439f7f47941c69d6dcd0e70dce469dc40
wangyihuai <wangyihuai1997@foxmail.com>
2018-11-08 14:48:56 +0800

    项目初始化

--- src/main/java/cn/com/pism/pismos/management/PismManagementApplication.java
+++ src/main/java/cn/com/pism/pismos/management/PismManagementApplication.java
@@ -1,4 +1,4 @@
-package cn.com.pism.pismos.management.pismmanagement;
+package cn.com.pism.pismos.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
