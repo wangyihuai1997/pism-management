3008b83c67f03a51d12606e67d0affe9a0e74008
wangyihuai <wangyihuai1997@foxmail.com>
2018-11-08 14:34:58 +0800

    项目初始化

---
+++ src/test/java/cn/com/pism/pismos/management/pismmanagement/PismManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.pism.pismos.management.pismmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PismManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
